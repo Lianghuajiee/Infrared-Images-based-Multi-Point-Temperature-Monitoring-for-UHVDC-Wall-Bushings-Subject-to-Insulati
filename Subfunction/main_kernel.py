de29e4b28258b84ecb42e40844a20410b3a97407
Lianghuajiee <13076649812@163.com>
2024-05-26 09:30:40 +0800

    .

---
+++ Subfunction/main_kernel.py
@@ -0,0 +1,227 @@
+import numpy as np
+import cv2 as cv
+from scipy.spatial import ConvexHull
+from matplotlib import pyplot as plt
+import torch
+import torch.nn as nn
+import torch.utils.data as Data
+import torchvision
+from torchvision.transforms import ToTensor
+import torch.nn.functional as F
+import os
+import sys
+import copy
+import matplotlib.image as mpimg
+import time
+import math
+from PIL import Image as IMG
+
+from Subfunction.Anomaly_detection import  Various_parameters
+from Subfunction.take_your_need import find_line_take_cannula_integration  ,piont_take_cannula,find_line_take_cannula
+from tqdm import tqdm 
+np.set_printoptions(threshold=1000000)
+
+def Polygon_extraction(image,Outcome_site):
+    Outcome_site = np.array([Outcome_site])
+    # 和原始图像一样大小的0矩阵，作为mask
+    mask = np.zeros(image.shape[:2], np.uint8)
+    # 在mask上将多边形区域填充为白色
+    cv.polylines(mask, Outcome_site, 1, 255,thickness=1)    # 描绘边缘
+    cv.fillPoly(mask, Outcome_site, 255)    # 填充
+    # 逐位与，得到裁剪后图像，此时是黑色背景
+    dst_back = cv.bitwise_and(image, image, mask=mask)
+
+     # 添加白色背景
+    bg = np.ones_like(image, np.uint8) * 255
+    cv.bitwise_not(bg, bg, mask=mask)  # bg的多边形区域为0，背景区域为255
+    dst_white = bg + dst_back
+    # cv.imshow("dst_white.jpg", dst_white)
+    return dst_back, dst_white
+
+def Convex_Hull (points):  #凸包顶点
+    hull = ConvexHull(points)
+
+    return points[hull.vertices]
+ 
+def get_regression (max_tmp,min_tmp):   #获取回归线
+    img_max = 255
+    img_min = 0
+    k = (max_tmp - min_tmp)/(img_max - img_min)
+    b = min_tmp - k * img_min
+    return k , b
+    
+def get_tmp (k , b , pixel):   #获取温度
+    return round(pixel * k + b, 2)
+    
+def Segmented_analysis (piont,x_size,y_size):   
+
+    piont_up_leng=np.unique(np.array(piont[:2]),axis=0)
+    piont_donw_leng=np.unique(np.array(piont[2:]),axis=0)
+    xpiont1 = int((piont_donw_leng[0,0]-piont_up_leng[0,0])/y_size)
+    ypiont1 = int((piont_donw_leng[0,1]-piont_up_leng[0,1])/y_size)
+    xpiont2 = int((piont_donw_leng[1,0]-piont_up_leng[1,0])/y_size)
+    ypiont2 = int((piont_donw_leng[1,1]-piont_up_leng[1,1])/y_size)
+    piont_Terminal  = []
+    piont_Terminal.append(piont_up_leng)
+    for i in range(1,y_size):
+        piont_Terminal.append([[piont_up_leng[0,0]+i*xpiont1,piont_up_leng[0,1]+i*ypiont1],[piont_up_leng[1,0]+i*xpiont2,piont_up_leng[1,1]+i*ypiont2]])
+    piont_Terminal.append(piont_donw_leng) 
+    piont_Terminal = np.array(piont_Terminal)    
+    
+    piont_out = []
+    
+    for piont_list in piont_Terminal:
+        x = (piont_list[1,0]-piont_list[0,0])/x_size
+        y = (piont_list[1,1]-piont_list[0,1])/x_size
+        qq = []
+
+        qq.append(piont_list[0])
+        for i in range(1,x_size):
+            qq.append([int(piont_list[0,0]+x*i),int(piont_list[0,1] + y*i)])
+        qq.append(piont_list[1])
+        piont_out.append(qq)
+    piont_out = np.array(piont_out)    
+   
+    return piont_out
+    
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+ 
+        self.hidden1 = nn.Linear(8 * 12, 128)
+        self.hidden2 = nn.Linear(128, 64)
+        self.output = nn.Linear(64, 13)
+    
+    def forward(self, x):
+
+        x = F.relu(self.hidden1(x))
+        x = F.relu(self.hidden2(x))
+        x = self.output(x)
+        return x
+def model_predict(path):
+
+    move_dir = ['0','1','2','3','4','5','6','7','8','9','.','-',' ']
+    max_tem_size=[8,12]             #数字高12，宽8
+    max_tem_piont = [37,7]
+    min_tem_piont = [37,21]
+    
+    img = cv.imread(path,0) 
+    img_max=img[max_tem_piont[1] : max_tem_piont[1]+max_tem_size[1] , max_tem_piont[0]:max_tem_piont[0]+max_tem_size[0]*5]
+    img_min=img[min_tem_piont[1] : min_tem_piont[1]+max_tem_size[1] , min_tem_piont[0]:min_tem_piont[0]+max_tem_size[0]*5]
+    max_data = ''
+    min_data = ''
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = MLP()
+    model.to(device)
+    if device== "cuda" :
+        model_name = 'mlp_model.pth'
+    else:
+        model_name = 'mlp_cpu_model.pth'
+    model.load_state_dict(torch.load('Data/'+model_name))
+    model.eval()
+    for i in range(5):
+        resized_image=img_max[:,max_tem_size[0]*i:max_tem_size[0]*(i+1)]
+        resized_image = resized_image.reshape(-1)   
+            
+        img_tensor = torch.tensor(resized_image, dtype=torch.float32).to(device)
+        img_tensor = img_tensor.unsqueeze(0)
+        with torch.no_grad():
+            outputs = model(img_tensor.float())
+        max_data += move_dir[outputs.argmax(1)]
+        
+        resized_image=img_min[:,max_tem_size[0]*i:max_tem_size[0]*(i+1)]
+        resized_image = resized_image.reshape(-1)   
+            
+        img_tensor = torch.tensor(resized_image, dtype=torch.float32).to(device)
+        img_tensor = img_tensor.unsqueeze(0)
+        with torch.no_grad():
+            outputs = model(img_tensor.float())
+        min_data += move_dir[outputs.argmax(1)]    
+    max_data = eval(max_data)
+    min_data = eval(min_data)
+    return max_data , min_data             
+
+
+def small_area_divide(image,image_gray,piont_up_mid_down,k,b,x_number,y_number,tt):
+    Small_area_temp_piont=[]
+    Small_area_piont=[]
+    Small_area_temp=[]
+    Small_area_max_temp = []
+    for i in range(x_number):
+        for o in range(y_number):
+            mid_area=[]
+            for j in range(2):
+                mid_area.append(piont_up_mid_down[0+o,i+j])
+                mid_area.append(piont_up_mid_down[1+o,i+j])
+            mid_area = np.array(mid_area)
+            Small_area_temp_piont.append(mid_area)
+            Small_area_piont.append(np.average(mid_area, axis=0))
+            if tt :
+                cv.line(image, mid_area[0], mid_area[1], (0,255,0), 1, cv.LINE_AA)
+                cv.line(image, mid_area[2], mid_area[3], (0,255,0), 1, cv.LINE_AA)
+                cv.line(image, mid_area[0], mid_area[2], (0,255,0), 1, cv.LINE_AA)
+                cv.line(image, mid_area[1], mid_area[3], (0,255,0), 1, cv.LINE_AA)
+            
+    
+    for itme in Small_area_temp_piont :             #可优化，减轻算力
+        local= []   
+        itme= Convex_Hull(itme)  
+        dst_gray_small_area, _=Polygon_extraction(image_gray,itme)
+        nonzero_coords = np.transpose(np.nonzero(dst_gray_small_area))     # 获取非零像素的坐标
+        for coord in nonzero_coords:
+            local.append(image_gray[coord[0], coord[1]])
+        Small_area_temp.append(get_tmp(k , b , np.mean(local)))
+        try:
+            Small_area_max_temp.append(get_tmp(k , b ,max(local)))
+        except:
+            Small_area_max_temp.append(999)
+
+    return Small_area_temp,Small_area_max_temp,Small_area_piont     
+def main_kernel():
+    piont_take_cannula.save_argument_to_txt()     
+    find_line_take_cannula.save_argument_to_txt()
+    path_list = os.listdir('Data/Raw_data/')
+    if not os.path.exists('Data/Generated_data/cut_list_acc/'): #判断所在目录下是否有该文件名的文件夹
+        os.mkdir('Data/Generated_data/cut_list_acc/')    
+    cut_list_nuber = [[2,5],[2,10]]
+    cut_list = ['one_to_one','two_to_one','three_to_one','four_to_one','five_to_one']   #'two_to_two','three_to_three'
+    for cut in cut_list:
+        temp_try_list = {"loca_small_mean_diff": {}, "loca_small_max_diff": {}}
+        temp_try_list["loca_small_mean_diff"] = {}
+        temp_try_list["loca_small_max_diff"] = {}
+        for path in tqdm(path_list): 
+            temp_try_list["loca_small_mean_diff"][path[:-4]] = {}  # 初始化子字典
+            temp_try_list["loca_small_max_diff"][path[:-4]] = {}  # 初始化子字典
+            max_tmp , min_tmp = model_predict('Data/Raw_data/'+path)
+            k , b =  get_regression(max_tmp,min_tmp)
+            image = cv.imread('Data/Raw_data/'+path)                     # 图片输入
+            img0 = cv.cvtColor(image,cv.COLOR_BGR2GRAY)         # 将彩色图片转换为灰度图片
+            try:   
+                Outcome_site = piont_take_cannula.work(path[:-4],1)      
+            except :
+                Outcome_site,si=find_line_take_cannula_integration(path[:10] ,copy.deepcopy(image),copy.deepcopy(img0),1)
+                Outcome_site = Outcome_site+ si
+            Casing_profile= Convex_Hull(Outcome_site)                     # 提取凸包顶点
+            dst_back_gray_all, _=Polygon_extraction(img0,Casing_profile)   # 提取红外套管区域图像，灰度图
+            dst_back_rgb, _=Polygon_extraction(image,Casing_profile) # 提取红外套管区域图像，红外图
+            filtered_image,local2 = Various_parameters.anomalydetect(copy.deepcopy(dst_back_gray_all),cut)
+            for cut_number in cut_list_nuber:
+                x_number = cut_number[1]
+                y_number = cut_number[0]
+                piont_up_mid_down = Segmented_analysis(Outcome_site,x_number,y_number)
+                Small_area_temp,Small_area_max_temp,_ = small_area_divide(image,copy.deepcopy(filtered_image),piont_up_mid_down,k,b,x_number,y_number,1)
+                if y_number == 1 :
+                    loca_small_mean_diff = max(Small_area_temp)-min(Small_area_temp)
+                    loca_small_max_diff = max(Small_area_max_temp)-min(Small_area_max_temp)
+                else:
+                    Small_area_temp_max0 = max(Small_area_temp[0::2]) - min(Small_area_temp[0::2])
+                    Small_area_temp_max1 = max(Small_area_temp[1::2]) - min(Small_area_temp[1::2])
+                    loca_small_mean_diff = max(Small_area_temp_max0,Small_area_temp_max1)
+                    loca_small_max_diff = max((max(Small_area_max_temp[0::2]) - min(Small_area_max_temp[0::2])),(max(Small_area_max_temp[1::2]) - min(Small_area_max_temp[1::2])))
+                temp_try_list["loca_small_mean_diff"][path[:-4]][str(y_number)+'*'+str(x_number)] = loca_small_mean_diff
+                temp_try_list["loca_small_max_diff"][path[:-4]][str(y_number)+'*'+str(x_number)] = loca_small_max_diff
+        with open('Data/Generated_data/cut_list_acc/'+cut+'.txt', 'w') as file:
+            for local, params in temp_try_list.items():
+                file.write(f"{local}: {params}\n")
+        
---
+++ Subfunction/matrix_two.py
@@ -0,0 +1,95 @@
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import seaborn as sns
+
+def Statistical_quantity_normel(loca_small_mean_diff,line):
+    loca_small_mean_diff = np.array(loca_small_mean_diff)
+    mask = loca_small_mean_diff <  line
+    count_per_column = np.sum(mask, axis=0)   
+    
+    return count_per_column
+  
+def Statistical_quantity_Breakdown(data_Fault_image,line):
+    data_Fault_image = np.array(data_Fault_image)
+    mask = data_Fault_image >=  line
+    count_per_column = np.sum(mask, axis=0)   
+    return count_per_column        
+    
+def get_acc (name_txt):
+    nan = 999
+    with open(name_txt, 'r') as file:
+        tr_pro_diff_normal = []
+        tr_pro_diff_Breakdown = []
+        loca_mean = []
+        loca_max = []
+        loca_small_mean_Breakdown = []
+        loca_small_max_Breakdown = []
+        for line in file:
+            if  'loca_small_mean_diff' in line[:35] :
+                line = line [len('loca_small_mean_diff')+2:]
+                data = eval(line)
+                data_2_5=[]
+                data_2_10=[]
+                loca_small_mean_Breakdowndata_2_5 = []
+                loca_small_mean_Breakdowndata_2_10 = []
+                for key, values in data.items():
+                    if key != 'local_15_0 (9)' :
+                        data_2_5.append(values['2*5'])
+                        data_2_10.append(values['2*10'])
+                    else :
+                        loca_small_mean_Breakdowndata_2_5.append(values['2*5'])
+                        loca_small_mean_Breakdowndata_2_10.append(values['2*10'])
+                record_tm = 2.7
+                loca_mean.append(np.array(Statistical_quantity_normel(data_2_5,record_tm)))
+                loca_mean.append(np.array(Statistical_quantity_normel(data_2_10,record_tm)))
+                loca_mean = np.array(loca_mean)
+                loca_small_mean_Breakdown.append(np.array(Statistical_quantity_Breakdown(loca_small_mean_Breakdowndata_2_5,record_tm)))
+                loca_small_mean_Breakdown.append(np.array(Statistical_quantity_Breakdown(loca_small_mean_Breakdowndata_2_10,record_tm)))
+                loca_small_mean_Breakdown = np.array(loca_small_mean_Breakdown)
+        if loca_small_mean_Breakdown[0] == 0 :
+            return_nenber = 0
+        else:
+            return_nenber = np.round((loca_mean[0]+loca_small_mean_Breakdown[0])/161 ,3)
+    return return_nenber
+
+
+
+
+
+def matrix_two():
+    model_list = ['One_svm', 'IsolationForest','EllipticEnvelope','DBSCAN', 'PCA','KMeans','HBOS','Z_Score','LocalOutlierFactor','Autoencoder']
+    acc_list = {}
+    number_list = []
+    for list1 in model_list:
+        acc_list[list1] = {}
+    for list1 in model_list:
+        for list2 in model_list:
+            if list1 == list2 :
+                acc_list[list1][list2] = get_acc('Data/Generated_data/one_acc/'+list1+'.txt')
+            else:
+                try:
+                    acc_list[list1][list2] = get_acc('Data/Generated_data/two_acc//'+list1+'_'+list2+'.txt')
+                    acc_list[list2][list1] = acc_list[list1][list2]
+                except:
+                    acc_list[list1][list2] = get_acc('Data/Generated_data/two_acc/'+list2+'_'+list1+'.txt')       
+                    acc_list[list2][list1] = acc_list[list1][list2]
+    acc_matrix = np.zeros((len(model_list), len(model_list)))
+    for i, model1 in enumerate(model_list):
+        for j, model2 in enumerate(model_list):
+            if model1 in acc_list and model2 in acc_list[model1]:
+                acc_matrix[i, j] = acc_list[model1][model2]
+    plt.figure(figsize=(10, 8))
+    plt.imshow(acc_matrix, interpolation='nearest', cmap='coolwarm')
+    plt.colorbar()
+    model_list = ['OneClassSVM', 'IsolationForest','EllipticEnvelope','DBSCAN', 'PCA','KMeans','HBOS','Z_Score','LocalOutlierFactor','Autoencoder']
+    plt.xticks(np.arange(len(model_list)), model_list, rotation=90)
+    plt.yticks(np.arange(len(model_list)), model_list)
+    for i in range(len(model_list)):
+        for j in range(len(model_list)):
+            plt.text(j, i, f'{acc_matrix[i, j]:.2f}', ha='center', va='center', color='white')
+    plt.title('Accuracy Matrix')
+    plt.xlabel('Models')
+    plt.ylabel('Models')
+    plt.savefig('Result/matrix.png') 
